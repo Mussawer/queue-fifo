499bc10de6bb143b1911fd0e626819e1bcf092a6
Mussawer <mgulzar.bscs15seecs@seecs.edu.pk>
2024-09-26 00:59:52 +0500

    add 2 functions - toArray that takes 0 arguments and returns an array that contains the data of queue - reverse function that takes 0 argument and returns queue in reversed order

--- index.js
+++ index.js
@@ -5,82 +5,81 @@
  */
 
 (function () {
-    'use strict';
+    "use strict";
 
     /***********************************************************
-     * Queue Data Structure
-     *
-     * This is a 'queue' data structure that implements the notion
-     * of a 'First in First Out', or FIFO, protocol.  The underlying data
-     * structure is a doubly linked list.  This linked list data structure
-     * does all the heavy lifting, enabling this implementation to be a
-     * simple wrapper around the linked list to leverage the applicable
-     * methods and properties.  This provides a very clean and simple
-     * implementation for this queue data structure.
-     *
-     ***********************************************************/
+   * Queue Data Structure
+   *
+   * This is a 'queue' data structure that implements the notion
+   * of a 'First in First Out', or FIFO, protocol.  The underlying data
+   * structure is a doubly linked list.  This linked list data structure
+   * does all the heavy lifting, enabling this implementation to be a
+   * simple wrapper around the linked list to leverage the applicable
+   * methods and properties.  This provides a very clean and simple
+   * implementation for this queue data structure.
+   *
+   ***********************************************************/
 
     // bring in the one dependency which will be the underlying
     // data structure for this queue implementation
-    var LinkedList = require('dbly-linked-list');
+    var LinkedList = require("dbly-linked-list");
 
     /**
-     * Creates a new queue instance and initializes the underlying data
-     * structure
-     *
-     * @constructor
-     */
+   * Creates a new queue instance and initializes the underlying data
+   * structure
+   *
+   * @constructor
+   */
     function Queue() {
         this._list = new LinkedList();
     }
 
     /* Functions attached to the Queue prototype.  All queue instances
-     * will share these methods, meaning there will NOT be copies made for each
-     * instance.  This will be a huge memory savings since there may be several
-     * different queue instances.
-     */
+   * will share these methods, meaning there will NOT be copies made for each
+   * instance.  This will be a huge memory savings since there may be several
+   * different queue instances.
+   */
     Queue.prototype = {
-
-        /**
-         * Determines if the queue is empty
-         *
-         * @returns {boolean} true if the queue is empty, false otherwise
-         */
+    /**
+     * Determines if the queue is empty
+     *
+     * @returns {boolean} true if the queue is empty, false otherwise
+     */
         isEmpty: function () {
             return this._list.isEmpty();
         },
 
         /**
-         * Returns the size, or number of items in the queue
-         *
-         * @returns {number} the number of items in the queue
-         */
+     * Returns the size, or number of items in the queue
+     *
+     * @returns {number} the number of items in the queue
+     */
         size: function () {
             return this._list.getSize();
         },
 
         /**
-         * Clears the queue of all data
-         */
+     * Clears the queue of all data
+     */
         clear: function () {
             return this._list.clear();
         },
 
         /**
-         * Adds a new item containing 'data' to the back of the queue
-         *
-         * @param {object} data the data to add to the back of the queue
-         */
+     * Adds a new item containing 'data' to the back of the queue
+     *
+     * @param {object} data the data to add to the back of the queue
+     */
         enqueue: function (data) {
             return this._list.insert(data);
         },
 
         /**
-         * Removes the item from the front of the queue.
-         *
-         * @returns {object} the item, or data, from the front of the queue.  If
-         *                   the list is empty, returns null.
-         */
+     * Removes the item from the front of the queue.
+     *
+     * @returns {object} the item, or data, from the front of the queue.  If
+     *                   the list is empty, returns null.
+     */
         dequeue: function () {
             if (this.isEmpty()) {
                 return null;
@@ -89,18 +88,43 @@
         },
 
         /**
-         * Returns the data of the item at the front of the queue,
-         * but does not remove it
-         *
-         * @returns {object} the item, or data, from the front of the queue.  If
-         *                   the list is empty, returns null.
-         */
+     * Returns the data of the item at the front of the queue,
+     * but does not remove it
+     *
+     * @returns {object} the item, or data, from the front of the queue.  If
+     *                   the list is empty, returns null.
+     */
         peek: function () {
             if (this.isEmpty()) {
                 return null;
             }
             return this._list.getHeadNode().getData();
-        }
+        },
+
+        // Converts the queue to an array, handling both node objects and direct data storage
+        toArray: function () {
+            var array = this._list.toArray();
+            if (array.length > 0 && typeof array[0].getData === 'function') {
+                // If the array contains nodes with getData method
+                return array.map(function (node) {
+                    return node.getData();
+                });
+            } else {
+                // If the array already contains the data
+                return array;
+            }
+        },
+
+        // Reverses the order of elements in the queue
+        reverse: function () {
+            var array = this._list.toArray();
+            var reversedArray = array.reverse();
+            this._list.clear();
+            var i;
+            for (i = 0; i < reversedArray.length; i++) {
+                this._list.insert(reversedArray[i]);
+            }
+        },
     };
 
     // export the constructor fn to make it available for use outside
